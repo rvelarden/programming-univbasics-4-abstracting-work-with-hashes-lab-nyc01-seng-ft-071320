d27dcd97c46a4c2b471cd5607d636391422d1950
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-14 19:54:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-hash_to_read = {location: 'New York City', occupation: 'Student'}
+hash_to_read = {key: 'value'}
 end
 
 def update_counting_hash(hash, key)
