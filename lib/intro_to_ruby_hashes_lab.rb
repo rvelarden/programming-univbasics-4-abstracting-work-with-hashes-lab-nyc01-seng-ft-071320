a5d580f8e4dffbdd10378b8adc78900f35f5c5ff
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-14 20:04:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-pets = {"cat" => "Maru", "dog" => "Pluto"}
+pets = {"cat" => "Maru"}
 #=> {"cat"=>"Maru", "dog"=>"Pluto"}
  
 pets["cat"]
